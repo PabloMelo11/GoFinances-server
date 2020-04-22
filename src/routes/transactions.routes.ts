224644791b0ddd42d45eefabc505d43dc1bdd73b
Pablo Melo <pablomelodev@gmail.com>
2020-04-22 01:22:11 -0300

    Create and list transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,53 @@
-import { Router } from 'express';
+import { Router, Response, Request } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
-  // TODO
+transactionsRouter.get('/', async (request: Request, response: Response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
-transactionsRouter.post('/', async (request, response) => {
-  // TODO
+transactionsRouter.post('/', async (request: Request, response: Response) => {
+  try {
+    const { title, value, type, category } = request.body;
+
+    const createTransaction = new CreateTransactionService();
+
+    const transaction = await createTransaction.execute({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.json({ error: err.message });
+  }
 });
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
-});
+transactionsRouter.delete(
+  '/:id',
+  async (request: Request, response: Response) => {
+    // TODO
+  },
+);
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  async (request: Request, response: Response) => {
+    // TODO
+  },
+);
 
 export default transactionsRouter;
