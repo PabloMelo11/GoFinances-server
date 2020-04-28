543c9eabe688e63a9cf33e8e1ab542cfbb33116f
Pablo Melo <pablomelodev@gmail.com>
2020-04-28 11:35:45 -0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,8 +12,9 @@ import createConnection from './database';
 
 createConnection();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
